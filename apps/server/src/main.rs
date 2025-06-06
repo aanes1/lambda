18ae0ca0c98600c441812678600e3cdd2eb391a0
aanes1 <anesen.niklas@gmail.com>
2025-06-06 04:56:56 +0200

    db init

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,11 +1,26 @@
 use axum::{Router, routing::get};
-use ld_core::api::mount;
+use dotenv::dotenv;
+use ld_core::api::{Ctx, mount};
+use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let db = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .unwrap();
+
+    let ctx = Ctx { db };
+
     let app = Router::new()
         .route("/", get(|| async { "lambda server!" }))
-        .merge(mount());
+        .merge(mount(ctx));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
--- core/src/api/mod.rs
+++ core/src/api/mod.rs
@@ -1,5 +1,13 @@
 use axum::{Router, routing::get};
+use sqlx::{Pool, Postgres};
 
-pub fn mount() -> Router {
-    Router::new().route("/health", get(|| async { "OK" }))
+#[derive(Clone)]
+pub struct Ctx {
+    pub db: Pool<Postgres>,
+}
+
+pub fn mount(ctx: Ctx) -> Router {
+    Router::new()
+        .route("/health", get(|| async { "OK" }))
+        .with_state(ctx)
 }
