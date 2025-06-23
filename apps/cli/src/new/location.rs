cb6adf4c269e1cd0fc0dd4fe9b35528a194f076f
aanes1 <anesen.niklas@gmail.com>
2025-06-24 01:55:55 +0200

    small change

--- apps/cli/src/new/location.rs
+++ apps/cli/src/new/location.rs
@@ -1,4 +1,3 @@
-use crate::utils;
 use anyhow::{Result, bail};
 use inquire::{Text, ui::RenderConfig, validator::Validation};
 use std::path::PathBuf;
@@ -22,7 +21,7 @@ pub fn from(loc: &str) -> Result<Location> {
         bail!("`{}` already exists", loc);
     }
 
-    let name = utils::get_name(&abs)?;
+    let name = get_name(&abs)?;
     let path = abs.with_file_name(&name);
 
     Ok(Location { path, name })
@@ -39,4 +38,33 @@ pub fn prompt(rcfg: RenderConfig) -> Result<Location> {
         .prompt()?;
 
     Ok(from(&loc)?)
+}
+
+pub fn get_name(abs: &PathBuf) -> Result<String> {
+    let file_name = abs.file_name().ok_or_else(|| {
+        anyhow::format_err!(
+            "cannot auto-detect project name from `{:?}`",
+            abs.as_os_str()
+        )
+    })?;
+
+    let file_name_str = file_name
+        .to_str()
+        .ok_or_else(|| anyhow::format_err!("`{:?}` cannot contain invalid unicode", file_name))?;
+
+    let name = file_name_str
+        .trim()
+        .to_lowercase()
+        .replace(" ", "-")
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric() || *c == '-' || *c == '_')
+        .collect::<String>()
+        .trim_matches(&['-', '_'])
+        .to_string();
+
+    if name.is_empty() {
+        bail!("project name must only contain alphanumeric characters, dashes, and underscores")
+    }
+
+    Ok(name)
 }
--- apps/cli/src/utils.rs
+++ apps/cli/src/utils.rs
@@ -1,36 +1,5 @@
-use anyhow::{Result, bail};
 use inquire::ui::RenderConfig;
-use std::path::PathBuf;
 
 pub fn get_render_config() -> RenderConfig<'static> {
     RenderConfig::empty()
-}
-
-pub fn get_name(abs: &PathBuf) -> Result<String> {
-    let file_name = abs.file_name().ok_or_else(|| {
-        anyhow::format_err!(
-            "cannot auto-detect project name from `{:?}`",
-            abs.as_os_str()
-        )
-    })?;
-
-    let file_name_str = file_name
-        .to_str()
-        .ok_or_else(|| anyhow::format_err!("`{:?}` cannot contain invalid unicode", file_name))?;
-
-    let name = file_name_str
-        .trim()
-        .to_lowercase()
-        .replace(" ", "-")
-        .chars()
-        .filter(|c| c.is_ascii_alphanumeric() || *c == '-' || *c == '_')
-        .collect::<String>()
-        .trim_matches(&['-', '_'])
-        .to_string();
-
-    if name.is_empty() {
-        bail!("project name must only contain alphanumeric characters, dashes, and underscores")
-    }
-
-    Ok(name)
 }
