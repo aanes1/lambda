836827c30c01f14e9ffad5e730656ddb3e6ef368
aanes1 <anesen.niklas@gmail.com>
2025-06-24 17:08:06 +0200

    framework

--- apps/cli/src/new/mod.rs
+++ apps/cli/src/new/mod.rs
@@ -1,12 +1,18 @@
+use std::str::FromStr;
+
 use crate::utils;
 use anyhow::Result;
 use clap::Parser;
+use framework::Framework;
 
+mod framework;
 mod location;
 
 #[derive(Parser)]
 pub struct NewArgs {
     location: Option<String>,
+    #[arg(long, alias = "fw")]
+    framework: Option<String>,
 }
 
 pub fn new(args: &NewArgs) -> Result<()> {
@@ -15,6 +21,11 @@ pub fn new(args: &NewArgs) -> Result<()> {
     let location = match &args.location {
         Some(loc) => location::from(loc)?,
         None => location::prompt(rcfg)?,
+    };
+
+    let framework = match &args.framework {
+        Some(s) => Framework::from_str(s)?,
+        None => framework::prompt(rcfg)?,
     };
 
     Ok(())
